1598ff39c5bf9f15efb85a88affc9f64865aa220
Andrew Santhosh C <andrewsanthosh264@gmail.com>
2023-10-25 15:46:16 +0530

    part 1 UserInterface

--- flask_server/static/js/scripts.js
+++ flask_server/static/js/scripts.js
@@ -1,28 +1,8 @@
-// Get the select element
-const langSelect = document.getElementById('langSelect');
-
-// Add an event listener for when the user selects a language
-langSelect.addEventListener('change', () => {
-// Get the selected language
-    const selectedLang = langSelect.value;
-
-// Hide all language elements
-    const langElems = document.querySelectorAll('.lang');
-    langElems.forEach(elem => {
-        elem.style.display = 'none';
-    });
-
-// Show the selected language element
-    const selectedElem = document.querySelector(`.lang.${selectedLang}`);
-    selectedElem.style.display = 'block';
-});
-
-
 // JavaScript code to handle user input and display chat
 const chatContainer = document.getElementById('chat-container');
 const submitButton = document.getElementById('submitButton');
 const userInput = document.getElementById('message');
-const image = document.getElementById('image');
+const image = document.getElementById('image-add-button');
 const socket = io.connect('http://' + location.hostname + ':' + location.port);
 
 submitButton.addEventListener('click', async (e) => {
@@ -110,54 +90,6 @@ function fetchBotMessage() {
         });
     });
 }
-
-
-function startSpeechRecognition() {
-    const recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.lang = 'en-US';
-
-    let timeoutId;
-
-    recognition.onresult = function (event) {
-        const speechResult = event.results[event.results.length - 1][0].transcript;
-        document.getElementById('message').value = speechResult;
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(function () {
-            recognition.stop();
-            startSpeechRecognition();
-        }, 3000);
-    };
-
-    recognition.onend = function () {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(function () {
-            startSpeechRecognition();
-        }, 3000);
-    };
-
-    recognition.start();
-}
-
-
-const voice = document.getElementsByClassName('speaker');
-
-function speak(text) {
-    const bott = document.getElementsByClassName('bot');
-    const utterance = new SpeechSynthesisUtterance(text);
-    speechSynthesis.speak(utterance);
-}
-
-window.onload = function () {
-    const listItems = document.querySelectorAll('.bot');
-    listItems.forEach(function (item) {
-        const button = document.createElement('button');
-        button.style.backgroundColor = 'transparent';
-        button.innerHTML = '<span class="speaker"><img src="../static/assets/volume.png" width="25px" ></span>';
-        button.addEventListener('click', function () {
-            speak(item.textContent);
-        });
-        item.appendChild(button);
-    });
-};
+document.getElementById('image-icon').addEventListener('click', function () {
+    document.getElementById('image-add-button').click();
+});
